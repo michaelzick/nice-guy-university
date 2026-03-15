2086964b98d05ed612a95745eb99b032c0afe89f
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2026-03-15 10:53:01 -0700

    fixed eslint errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
 	darkMode: ["class"],
@@ -103,5 +105,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+	plugins: [tailwindcssAnimate, typography],
 } satisfies Config;
